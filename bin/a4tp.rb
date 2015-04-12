6f5b0037a46509733965f39e16cd115e54b0802d
Shayne <sclausson@hotmail.com>
2015-04-12 09:55:18 +0200

    corrects syntax

--- bin/a4tp.rb
+++ bin/a4tp.rb
@@ -5,16 +5,16 @@ require 'trollop'
 @opts = Trollop::options do
 	opt :stackName, "Unique name of the CloudFormation stack used to create a4tp resources", :type => String, :required => true
 	opt :template, "Name of the Cfn Template that describes our infrastructure", :type => String, :required => true
-	opt :keyname, "Name of a valid EC2 keypair that can be used to access the instances",	:type => String, :required => true
-	opt :instancetype, "The type of instance to launch (ex. m3.medium)", :type => String, :required => true
+	opt :keyName, "Name of a valid EC2 keypair that can be used to access the instances",	:type => String, :required => true
+	opt :instanceType, "The type of instance to launch (ex. m3.medium)", :type => String, :required => true
 end
 
 @stack = OpenDelivery::Stack.new
 
 def create_params
 	params = {
-		"KeyName" => @opts[:keyname], 
-		"InstanceType" => @opts[:instancetype]
+		"KeyName" => @opts[:keyName], 
+		"InstanceType" => @opts[:instanceType]
 	}
 end
 
