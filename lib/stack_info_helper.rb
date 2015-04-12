d36c302bc0a0a46487b372855dc68bee2e29ecd0
Shayne <sclausson@hotmail.com>
2015-04-12 20:23:40 +0200

    Adds a lib file with methods for grabbing ASG and IP info for a stack.

---
+++ lib/stack_info_helper.rb
@@ -0,0 +1,47 @@
+#provides a couple of helpful methods we can call for getting stack resource info
+require 'aws-sdk'
+
+class Stack
+
+  attr_reader :stack_name, :asg, :ips
+
+  def initialize(stack_name)
+    @stack_name = stack_name
+    get_stack_asg
+    get_ip_addresses
+  end
+
+  def get_stack_asg
+      cfn = AWS::CloudFormation.new
+      as = AWS::AutoScaling.new
+
+      stack = cfn.stacks[@stack_name]
+      stack.resources.each do |resource|
+        case resource.resource_type
+        when "AWS::AutoScaling::AutoScalingGroup"
+          asg_id = resource.physical_resource_id
+          @asg = as.groups[asg_id]
+        end
+      end
+      return @asg
+  end
+
+  def get_ip_addresses
+    cfn = AWS::CloudFormation.new
+    as = AWS::AutoScaling.new
+    ec2 = AWS::EC2.new
+
+    @instanceCollections = []
+    asg = get_stack_asg
+    @instanceCollections << asg.ec2_instances
+
+    @ips = []
+
+    @instanceCollections.each do |ic|
+      ic.each do |instance|
+        @ips << instance.private_ip_address
+      end
+    end
+    return @ips
+  end
+end
