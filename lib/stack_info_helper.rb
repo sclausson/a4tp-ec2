6799535e1a54212a0087a78dbe7e8f3036d76b18
Shayne <sclausson@hotmail.com>
2015-04-20 21:22:13 +0200

    refactor stack_info_helper

--- lib/stack_info_helper.rb
+++ lib/stack_info_helper.rb
@@ -16,35 +16,22 @@ class Stack
   end
 
   def get_stack_asg
-      stack = @@cfn.stacks[@stack_name]
-      stack.resources.each do |resource|
-        case resource.resource_type
-        when "AWS::AutoScaling::AutoScalingGroup"
-          asg_id = resource.physical_resource_id
-          @asg = @@as.groups[asg_id]
-        end
-      end
-      return @asg
+    stack = @@cfn.stacks[@stack_name]
+    asg_resource = stack.resources.find { |r| r.resource_type == "AWS::AutoScaling::AutoScalingGroup" }
+    asg_id = asg_resource.physical_resource_id
+    @asg = @@as.groups[asg_id]
   end
 
   def get_ip_addresses
-    @ips = []
-    @asg.ec2_instances.each do |i|
-        @ips << i.private_ip_address
-    end
-    return @ips
+    @ips = @asg.ec2_instances.map { |i| i.private_ip_address }
   end
 
   def get_hostnames
-    @hostnames = []
-    @asg.ec2_instances.each do |i|
-        @hostnames << i.private_dns_name.split('.').first
-    end
-    return @hostnames
+    @hostnames = @asg.ec2_instances.map { |i| i.private_dns_name.split('.').first }
   end
 
   def output_value(output_key)
-    output = @@cfn.stacks["#{@stack_name}"].outputs.select {|o| o.key == "#{output_key}"}
+    output = @@cfn.stacks["#{@stack_name}"].outputs.select { |o| o.key == "#{output_key}" }
     return output.first.value
   end
 
